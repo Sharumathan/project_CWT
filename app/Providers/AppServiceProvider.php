dc6dfb0435976ae2390638590faf2c61e17e5187
Sharumathan <163159851+Sharumathan@users.noreply.github.com>
2026-01-10 17:23:49 +0530

    Fix mixed content errors by forcing HTTPS

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -16,6 +16,10 @@ class AppServiceProvider extends ServiceProvider
 
     public function boot(): void
     {
+        if (config('app.env') !== 'local') {
+            \Illuminate\Support\Facades\URL::forceScheme('https');
+        }
+
         try {
             if (Schema::hasTable('system_config')) {
                 $dbAdminEmail = DB::table('system_config')
